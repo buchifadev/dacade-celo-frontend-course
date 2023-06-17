7ea025d613ce89b83cf96c032f5abecbc3a44607
Favour <favobuchi@outlook.com>
2023-06-17 22:43:39 +0100

    Improved inline code context

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,6 +1,6 @@
 // This is the main page of the app
 
-// Import the AddProductModal and ProductList components
+// Import the AddProductModal, ProductList, Dashboard, and DisplayBalance components
 import AddProductModal from "@/components/AddProductModal";
 import ProductList from "@/components/ProductList";
 import Dashboard from "@/components/Dashboard";
@@ -10,13 +10,18 @@ import DisplayBalance from "@/components/DisplayBalance";
 export default function Home() {
   return (
     <div>
+      {/* Render the components imported above */}
       <div className="flex justify-between">
         <div className="flex items-center justify-between">
+          {/* Render the AddProductMdoal component */}
           <AddProductModal/>
+          {/* Render the Dashboard component */}
           <Dashboard/>
         </div>
+        {/* Render the DisplayBalance component */}
           <DisplayBalance/>
       </div>
+      {/* Render the ProductList component */}
         <ProductList />
     </div>
   )
